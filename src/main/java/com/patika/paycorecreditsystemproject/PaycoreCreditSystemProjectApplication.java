9fc9cdaa3a027ab498ef9f9b822b6daf4661a0ca
aleynazengin <aleystyles23@gmail.com>
2022-02-18 18:26:15 +0300

    project initialized

---
+++ src/main/java/com/patika/paycorecreditsystemproject/PaycoreCreditSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.patika.paycorecreditsystemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaycoreCreditSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaycoreCreditSystemProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patika/paycorecreditsystemproject/PaycoreCreditSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.paycorecreditsystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaycoreCreditSystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
