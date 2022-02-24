956c025a14bbb4b615d84f04baeb88df1156cc83
aleynazengin <aleystyles23@gmail.com>
2022-02-25 02:12:09 +0300

    CreditApplicationController added

---
+++ src/main/java/com/patika/paycorecreditsystemproject/controller/CreditApplicationController.java
@@ -0,0 +1,39 @@
+package com.patika.paycorecreditsystemproject.controller;
+
+import com.patika.paycorecreditsystemproject.model.CreditApplication;
+import com.patika.paycorecreditsystemproject.model.dto.CreditApplicationDTO;
+import com.patika.paycorecreditsystemproject.service.CreditApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.List;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/creditApplication")
+public class CreditApplicationController {
+    private final CreditApplicationService creditApplicationService;
+    int limit;
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Credit Application Service!";
+    }
+
+    @PostMapping(value = "/applyForCredit")
+    public String createCreditApplication(@Valid @RequestBody CreditApplicationDTO creditApplicationDTO) {
+        creditApplicationService.addCreditApplication(creditApplicationDTO);
+        return creditApplicationService.getResult(creditApplicationDTO.getNationalId());
+
+    }
+
+    @GetMapping("/by-customer/{nationalId}")
+    public List<CreditApplication> getAllApplicationByCustomer(@PathVariable String nationalId) {
+        return creditApplicationService.getAllApplicationByCustomer(nationalId);
+    }
+
+}
